07db11616dc680f66bc19eaa9b9410bc7da3d429
Jack Lin <43402335+snippaxkilla@users.noreply.github.com>
2024-03-07 12:23:04 +0100

    fixes

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -15,9 +15,14 @@ public class RoomSpawner : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("SpawnPoint") && collision.GetComponent<RoomSpawner>().spawned == true)
+        if (collision.CompareTag("SpawnPoint"))
         {
-            Destroy(gameObject);
+            if (collision.GetComponent<RoomSpawner>().spawned == false && spawned == false)
+            {
+                //Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            spawned = true;      
         }
     }
 
